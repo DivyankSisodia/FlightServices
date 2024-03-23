1aa337a3e37864943792c147d8c824d222c5563a
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-23 23:41:03 +0530

    Completed City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,50 @@
 const { City } = require('../models/index');
 
-class CityRepository{
-    async createCity({name}){
-        try{
-            const city = await City.create({name});
+class CityRepository {
+    async createCity({ name }) {
+        try {
+            const city = await City.create({ name });
             return city;
         }
-        catch(err){
-            throw {err}
+        catch (err) {
+            throw { err }
         }
     }
 
-    async deleteCity(cityId){
-        try{
+    async deleteCity(cityId) {
+        try {
             await City.destroy({
                 where: {
                     id: cityId,
                 }
             });
+            return true;
         }
-        catch(err){
-            throw {err}
+        catch (err) {
+            throw { err }
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            await City.update(data, {
+                where: {
+                    id: cityId,
+                }
+            });
+            return true;
+        }
+        catch (err) {
+            throw { err }
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            throw { error }
         }
     }
 }
